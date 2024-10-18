76b8a6c69ff52e264cf0b7523cf44b41653c4d7e
Gabriele Tosto <g.tosto@flazio.com>
2024-10-18 22:20:17 +0200

    fix

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,10 +4,15 @@ import "./Hero.css";
 
 function Hero() {
   return (
-    <div id="heroComponents" className="hero d-flex flex-column justify-content-center align-items-center">
-      <Image src="/hero-images/korat-logo.png" alt="Logo Sparking Zero Italia" className="img-fluid d-none d-lg-block" width={467} height={131}></Image>
-      <Image src="/hero-images/korat-logo.png" alt="Logo Sparking Zero Italia" className="img-fluid d-lg-none" width={300} height={131}></Image>
+    <div id="heroComponents" className="hero d-flex flex-column justify-content-center">
+      <div className="d-flex flex-column justify-content-center align-items-right ms-5 ps-5">
+        <Image src="/hero-images/korat-logo.png" alt="Logo Sparking Zero Italia" className="img-fluid d-none d-lg-block logo-position ms-5 ps-5" width={467} height={131}></Image>
+      </div>
+      <div className="d-flex flex-column justify-content-center align-items-center">
+        <Image src="/hero-images/korat-logo.png" alt="Logo Sparking Zero Italia" className="img-fluid d-lg-none" width={300} height={131}></Image>
+      </div>
     </div>
+    
   );
 }
 
