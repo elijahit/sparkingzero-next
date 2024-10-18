0cc18590660cd7c9d25dfc35b87041fdaa30eaf1
Gabriele Tosto <g.tosto@flazio.com>
2024-10-18 21:49:25 +0200

    test

--- scripts/database.js
+++ scripts/database.js
@@ -1,14 +1,11 @@
 const sqlite3 = require("sqlite3").verbose();
 const {open} = require("sqlite");
-const path = require("path");
 
 
   // open the database
   const db = await open({
-    filename: 'databaseSparking.db',
+    filename: 'database.db',
     driver: sqlite3.Database
   })
   export default db;
-
-  const post = await db.get("select titolo from article where titolo = ?", "titolo");
 
